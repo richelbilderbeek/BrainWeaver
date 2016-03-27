cfe9f089c1f695995a7b0627ba23af3d5c69433f
richelbilderbeek <richel@richelbilderbeek.nl>
2016-03-27 16:05:50 +0200

    EOD

--- brainweaverfilefactory_test.cpp
+++ brainweaverfilefactory_test.cpp
@@ -14,11 +14,11 @@ BOOST_AUTO_TEST_CASE(ribi_pvdb_filefactory_test)
 {
   using namespace ribi::pvdb;
   FileFactory f;
-  if (f.GetNumberOfTests() != static_cast<int>(f.GetTests().size()))
-  {
-    std::cerr << "Change FileFactory::GetNumberOfTests to return " << f.GetTests().size() << std::endl;
-    TRACE(f.GetNumberOfTests());
-    TRACE(f.GetTests().size());
-  }
-  BOOST_CHECK(f.GetNumberOfTests() == static_cast<int>(f.GetTests().size()));
+  //if (f.GetNumberOfTests() != static_cast<int>(f.GetTests().size()))
+  //{
+  //  std::cerr << "Change FileFactory::GetNumberOfTests to return " << f.GetTests().size() << std::endl;
+  //  TRACE(f.GetNumberOfTests());
+  //  TRACE(f.GetTests().size());
+  //}
+  BOOST_CHECK_EQUAL(f.GetNumberOfTests(), static_cast<int>(f.GetTests().size()));
 }
