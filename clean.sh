d7c571b6cde24c3689bbbb3a381223f332ad412c
richelbilderbeek <richel@richelbilderbeek.nl>
2016-04-19 12:59:59 +0200

    Only remove existing files

--- clean.sh
+++ clean.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 rm moc_*
-rm object_script*.*
 rm ui_*
 rm qrc_*
 rm *.gcov
 rm *.gcda
 rm *.gcno
 rm *.o
-rm *_plugin_import.cpp
-rm *.pro.user
 rm Makefile
-rm Makefile.Debug
-rm Makefile.Release
-rm tmp*
