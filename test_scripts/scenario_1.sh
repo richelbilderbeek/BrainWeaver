24a85945eb00c36f70bc2019e4616323dc88cdb0
richelbilderbeek <richel@richelbilderbeek.nl>
2016-05-08 12:05:39 +0200

    scneatio1 works

--- test_scripts/scenario_1.sh
+++ test_scripts/scenario_1.sh
@@ -1,42 +1,144 @@
 #!/bin/bash
 # Let the assessor create a test concept map
 
+
+# Load scripts
+. ../../testing_cpp_gui_applications_tutorial/scripts/get_dialog_id.sh
+
+# Variables
 myexe="../../build-BrainweaverDeveloper-Desktop-Debug/BrainweaverDeveloper"
-if [ -e /$myexe ]
+mycmp="scenario1.cmp"
+
+####################################
+# Check executable
+####################################
+if [ -e /$myexe ] # slash is obligatory?
 then
   echo $mytestname": FAIL (executable '"$myexe"' not found)"
   exit
 fi
 
+####################################
+# Delete saving file
+####################################
+if [ -e $mycmp ]
+then
+ echo "Warning: file '"$mycmp"' existed, but is now deleted"
+  rm $mycmp
+fi
+if [ -e $mycmp ]
+then
+  echo "File could not be deleted, line "$LINENO
+  exit 1
+fi
+
+####################################
 # Start application
+####################################
 $myexe &
 sleep 1
 
-# Main menu, click 'Asessor'
-# note: xdotool cannot find the window from its title
-# xdotool windowactivate --sync "$(xdotool search --name "Menu voor de ontwikkelaar" | head -n 1)" key alt+1
-# Click
-xdotool mousemove 451 287 click 1
-sleep 1
+####################################
+# Main menu, choose '2. Assessor'
+####################################
+id=`get_dialog_id "Menu voor de ontwikkelaar"`
+if [ -z $id ]
+then
+  echo "ID not found, line "$LINENO
+  exit 1
+fi
+xdotool windowactivate $id key alt+2 sleep 0.2
 
-# Click 'Geef assessment vorm'
-xdotool mousemove 634 515 click 1
-sleep 1
+####################################
+# 'Mijn persoonlijke werktheorie', choose '&Geef assessment vorm'
+####################################
+id=`get_dialog_id "Mijn persoonlijke werktheorie, programma voor de assessor"`
+if [ -z $id ]
+then
+  echo "ID not found, line "$LINENO
+  exit 1
+fi
+xdotool windowactivate $id key alt+g sleep 0.2
 
-# Click 'Create one'
-xdotool mousemove 315 394 click 1
+####################################
+# 'Geef assessment vorm: wat voor assessment wilt u maken?', 
+# choose '&In een keer zijn/haar complete concept map contrueert?'
+####################################
+id=`get_dialog_id "Geef assessment vorm: wat voor assessment wilt u maken?"`
+if [ -z $id ]
+then
+  echo "ID not found, line "$LINENO
+  exit 1
+fi
+xdotool windowactivate $id key alt+i sleep 0.2
 
-sleep 2
+####################################
+# 'Formuleer de focusvraag', 
+# type a focal question
+####################################
+id=`get_dialog_id "Formuleer de focusvraag"`
+if [ -z $id ]
+then
+  echo "ID not found, line "$LINENO
+  exit 1
+fi
+xdotool windowactivate $id type "Good focal questions are ..."
 
-#xdotool windowactivate --sync "$(xdotool search --name "Kies een assessment bestand" | head -n 1)" key ctrl+a; xdotool windowactivate --sync "$(xdotool search --name "Kies een assessment bestand" | head -n 1)" key KP_Delete; xdotool type --window "$(xdotool search --name "Kies een assessment bestand" | head -n 1)" --delay 1 test.cmp; xdotool windowactivate --sync "$(xdotool search --name "Kies een assessment bestand" | head -n 1)" key KP_Enter
+####################################
+# 'Formuleer de focusvraag', 
+# save
+####################################
+id=`get_dialog_id "Formuleer de focusvraag"`
+if [ -z $id ]
+then
+  echo "ID not found, line "$LINENO
+  exit 1
+fi
+xdotool windowactivate $id sleep 0.2 key alt+s sleep 0.3
+
+####################################
+# 'Sla het assessment invoer-bestand op', 
+# save as scenario_1
+####################################
+id=`get_dialog_id "Sla het assessment invoer-bestand op"`
+if [ -z $id ]
+then
+  echo "ID not found, line "$LINENO
+  exit 1
+fi
+xdotool windowactivate $id sleep 0.2 type $mycmp
+xdotool windowactivate $id sleep 0.2 key alt+o sleep 0.2
+
+####################################
+# 'Mijn persoonlijke werktheorie', choose '&Stoppen'
+####################################
+id=`get_dialog_id "Mijn persoonlijke werktheorie, programma voor de assessor"`
+if [ -z $id ]
+then
+  echo "ID not found, line "$LINENO
+  exit 1
+fi
+xdotool windowactivate $id key alt+s sleep 0.2
+
+####################################
+# Checking saved file
+####################################
+if [ ! -e $mycmp ]
+then
+  echo "File should have been created, line "$LINENO
+  exit 1
+fi
+
+####################################
+# Main menu, close
+####################################
+id=`get_dialog_id "Menu voor de ontwikkelaar"`
+if [ -z $id ]
+then
+  echo "ID not found, line "$LINENO
+  exit 1
+fi
+xdotool windowactivate $id key alt+F4
 
 
-
-
-
-
-
-# Notes
-
-# Get mouse position
-xdotool getmouselocation
+echo "File '"$mycmp"' is created successfully"
