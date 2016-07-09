8742c9fb32b94c1ad28d4bf62e7e30e547535539
richelbilderbeek <richel@richelbilderbeek.nl>
2016-07-09 13:15:42 +0200

    Added #include

--- brainweaverclusterfactory.h
+++ brainweaverclusterfactory.h
@@ -25,8 +25,7 @@ along with this program.If not, see <http://www.gnu.org/licenses/>.
 #pragma GCC diagnostic ignored "-Weffc++"
 #pragma GCC diagnostic ignored "-Wunused-local-typedefs"
 #pragma GCC diagnostic ignored "-Wunused-but-set-parameter"
-#include <boost/shared_ptr.hpp>
-
+#include <vector>
 #include "brainweaverfwd.h"
 #pragma GCC diagnostic pop
 
