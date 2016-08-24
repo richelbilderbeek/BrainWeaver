2746b4ca70d2baa4ccb07112043cbfc787c79111
richelbilderbeek <richel@richelbilderbeek.nl>
2016-08-24 16:16:53 +0200

    EOD

--- qtmain_test.cpp
+++ qtmain_test.cpp
@@ -45,7 +45,6 @@ int main(int argc, char *argv[])
   { ribi::braw::qtbrainweaverstudentmenudialog_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::braw::qtbrainweaverstudentstartcompletedialog_test t; error |= QTest::qExec(&t, argc, argv); }
 
-
   if (error == 0) { std::cout << "Pass\n"; }
   if (error != 0) { std::cout << "Fail\n"; }
   return error;
