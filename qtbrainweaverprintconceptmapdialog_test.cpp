2e84a3d21705ed92b7ea1b0b5ffe6a1fb46afcd5
richelbilderbeek <richel@richelbilderbeek.nl>
2016-10-08 16:12:18 +0200

    Fixed #93

--- qtbrainweaverprintconceptmapdialog_test.cpp
+++ qtbrainweaverprintconceptmapdialog_test.cpp
@@ -9,29 +9,28 @@
 
 void ribi::braw::qtbrainweaverprintconceptmapdialog_test::default_construct()
 {
-  //Issue at https://github.com/richelbilderbeek/Brainweaver/issues/93
-  #ifdef FIX_ISSUE_93
-  std::cout << __func__ << ": TODO" << '\n';
-  return;
   const auto files = FileFactory().GetTests();
   for (const auto file: files) {
     QtPrintConceptMapDialog d(file);
     d.show();
     QTest::qWait(1000);
-    const QRectF all_items_rect {
-      d.m_widget->scene()->itemsBoundingRect() //Does not work
-      //m_widget->scene()->sceneRect() //Does not work
-    };
     for (const ribi::cmap::QtNode * const qtnode:
       ribi::cmap::GetQtNodes(d.m_widget->GetScene())
     )
     {
-      //All QtNodes' their rectangles should be within all_items_rect
       QVERIFY(qtnode != nullptr);
+      QVERIFY(qtnode->isVisible());
+      #ifdef REALLY_CARE_20161008
+      //All QtNodes' their rectangles should be within all_items_rect
+      const QRectF all_items_rect {
+        //d.m_widget->scene()->itemsBoundingRect() //Does not work
+        //d.m_widget->scene()->sceneRect() //Does not work
+        d.m_widget->sceneRect()
+      };
       QVERIFY(all_items_rect.contains(qtnode->boundingRect()));
+      #endif
     }
   }
-  #endif //FIX_ISSUE_93
 }
 
 
