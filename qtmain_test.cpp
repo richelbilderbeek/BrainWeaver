043be1ebd8344e473185548564d4badc461c1004
richelbilderbeek <richel@richelbilderbeek.nl>
2016-10-16 12:23:52 +0200

    Codecov

--- qtmain_test.cpp
+++ qtmain_test.cpp
@@ -25,8 +25,6 @@ int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   int error = 0;
-  { ribi::braw::qtbrainweavercreateassessmentcompletedialog_test t; error |= QTest::qExec(&t, argc, argv); }
-  return 1;
 
   { ribi::braw::qtbrainweaveraboutdialog_test t; error |= QTest::qExec(&t, argc, argv); }
   { ribi::braw::qtbrainweaverassessormenudialog_test t; error |= QTest::qExec(&t, argc, argv); }
