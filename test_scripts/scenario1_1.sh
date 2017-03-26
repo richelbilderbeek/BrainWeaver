cb4004a80a78199f723b6591b7a7b3bc91203238
richelbilderbeek <richel@richelbilderbeek.nl>
2017-03-26 18:37:57 +0200

    EOD

--- test_scripts/scenario1_1.sh
+++ test_scripts/scenario1_1.sh
@@ -3,7 +3,7 @@
 # Assumes BrainweaverDeveloper is already running
 
 # Load scripts
-. ../../testing_cpp_gui_applications_tutorial/scripts/get_dialog_id
+. ../../testing_cpp_gui_applications_tutorial/scripts/get_dialog_id_by_title
 
 # Variables
 mycmp="scenario1.cmp"
@@ -20,7 +20,7 @@ fi
 ####################################
 # Main menu, choose '2. Assessor'
 ####################################
-id=`get_dialog_id "Menu voor de ontwikkelaar"`
+id=`get_dialog_id_by_title "Menu voor de ontwikkelaar"`
 if [ -z $id ]
 then
   echo "ID not found, line "$LINENO
@@ -31,7 +31,7 @@ xdotool windowactivate $id key alt+2 sleep 0.2
 ####################################
 # 'Mijn persoonlijke werktheorie', choose '&Geef assessment vorm'
 ####################################
-id=`get_dialog_id "Mijn persoonlijke werktheorie, programma voor de assessor"`
+id=`get_dialog_id_by_title "Mijn persoonlijke werktheorie, programma voor de assessor"`
 if [ -z $id ]
 then
   echo "ID not found, line "$LINENO
@@ -43,7 +43,7 @@ xdotool windowactivate $id key alt+g sleep 0.2
 # 'Formuleer de focusvraag', 
 # type a focal question
 ####################################
-id=`get_dialog_id "Formuleer de focusvraag"`
+id=`get_dialog_id_by_title "Formuleer de focusvraag"`
 if [ -z $id ]
 then
   echo "ID not found, line "$LINENO
@@ -57,7 +57,7 @@ xdotool windowactivate $id sleep 0.2 key alt+s sleep 1.0
 # 'Sla het assessment invoer-bestand op', 
 # save as scenario_1
 ####################################
-id=`get_dialog_id "Sla het assessment invoer-bestand op"`
+id=`get_dialog_id_by_title "Sla het assessment invoer-bestand op"`
 if [ -z $id ]
 then
   echo "ID not found, line "$LINENO
@@ -69,7 +69,7 @@ xdotool windowactivate $id sleep 0.2 key Return sleep 1.0
 ####################################
 # 'Mijn persoonlijke werktheorie', choose '&Stoppen'
 ####################################
-id=`get_dialog_id "Mijn persoonlijke werktheorie, programma voor de assessor"`
+id=`get_dialog_id_by_title "Mijn persoonlijke werktheorie, programma voor de assessor"`
 if [ -z $id ]
 then
   echo "ID not found, line "$LINENO
