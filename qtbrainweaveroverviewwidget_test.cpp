ad81de94cf0eae16ab9599d02db2db886bf8700f
richelbilderbeek <richel@richelbilderbeek.nl>
2018-08-03 11:33:02 +0200

    Only test QtOverviewWidget locally

--- qtbrainweaveroverviewwidget_test.cpp
+++ qtbrainweaveroverviewwidget_test.cpp
@@ -3,9 +3,12 @@
 
 void ribi::braw::qtbrainweaveroverviewwidget_test::default_construction()
 {
-  ribi::braw::QtOverviewWidget d;
-  d.show();
-  QTest::keyClick(&d, Qt::Key_X);
-  QTest::keyClick(&d, Qt::Key_Escape);
+  if (!ribi::cmap::OnTravis())
+  {
+    ribi::braw::QtOverviewWidget d;
+    d.show();
+    QTest::keyClick(&d, Qt::Key_X);
+    QTest::keyClick(&d, Qt::Key_Escape);
+  }
 }
 
