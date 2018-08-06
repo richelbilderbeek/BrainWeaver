5ea6f25840ffe7f9f08bc9a916de5540844b3f2e
richelbilderbeek <richel@richelbilderbeek.nl>
2018-08-06 19:21:42 +0200

    Remove mention of unused classes, update date

--- brainweavermenudialog.cpp
+++ brainweavermenudialog.cpp
@@ -6,7 +6,6 @@
 #include "conceptmap.h"
 #include "container.h"
 #include "fileio.h"
-#include "fuzzy_equal_to.h"
 #include "geometry.h"
 #include "ribi_regex.h"
 #include "xml.h"
@@ -29,17 +28,15 @@ ribi::About ribi::braw::MenuDialog::GetAbout() const noexcept
     "Richel Bilderbeek",
     "BrainWeaver",
     "tool to create and assess concept maps",
-    "June 12th of 2016",
-    "2012-2016",
+    "August 6th of 2018",
+    "2012-2018",
     "http://www.richelbilderbeek.nl/ProjectBrainWeaver.htm",
     GetVersion(),
     GetVersionHistory());
 
   a.AddLibrary("apfloat version: 2.4.1");
-  //a.AddLibrary("ConceptMap version: " + ribi::cmap::ConceptMap::GetVersion());
   a.AddLibrary("Container version: " + ribi::Container().GetVersion());
   a.AddLibrary("FileIo version: " + ribi::fileio::FileIo().GetVersion());
-  a.AddLibrary("fuzzy_equal_to version: " + fuzzy_equal_to::GetVersion());
   a.AddLibrary("Geometry version: " + Geometry().GetVersion());
   a.AddLibrary("ribi::Regex version: " + ribi::Regex::GetVersion());
   return a;
